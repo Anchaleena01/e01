472a33d8c731d51535fbb45dd97a1a3d2884e14c
anchana <anchanamanoj2001@gmail.com>
2024-02-26 16:29:58 +0530

    comitted

--- ecproject/src/ecproject/Gpro.java
+++ ecproject/src/ecproject/Gpro.java
@@ -1,10 +1,15 @@
 package ecproject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Gpro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\ancha\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com");
 	}
 
 }
